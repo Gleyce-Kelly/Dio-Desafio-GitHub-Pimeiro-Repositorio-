688836e5873c8a9d1fc63806f5204ec9f4b8bced
Gleyce-Kelly <gleyce_180@outlook.com>
2022-06-21 14:54:15 -0300

    Módulos em JavaScript

---
+++ JavaScript/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gleyce", 23));
+console.log(mostraCidade("Gleyce", "Mauá"));
+console.log(mostraHobby("Gleyce", "Assistir Filmes"));
